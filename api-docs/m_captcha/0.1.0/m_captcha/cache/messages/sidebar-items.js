910f66d22dee9f8355083fa709d4836021183bd8
realaravinth <realaravinth@users.noreply.github.com>
2021-03-11 05:08:07 +0000

    deploy: c1f7be6994e29515ebead78e56865e4edce5680f

---
+++ api-docs/m_captcha/0.1.0/m_captcha/cache/messages/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Cache","Message to cache PoW difficulty factor and string"],["CacheBuilder","Builder for `Cache`."],["DeleteString","Message to delete cached PoW difficulty factor and string when they expire"],["Retrive","Message to retrive the the difficulty factor for the specified string from the cache"]]});
