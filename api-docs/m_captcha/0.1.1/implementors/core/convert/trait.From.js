afca9d6ad2ab0cc7f7579f5377bade07f0397a3c
realaravinth <realaravinth@users.noreply.github.com>
2022-11-06 11:59:10 +0000

    deploy: d970b882fa9520f6acfde7d27100a06eb2d5df26

---
+++ api-docs/m_captcha/0.1.1/implementors/core/convert/trait.From.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["m_captcha"] = [{"text":"impl From&lt;Work&gt; for PoW&lt;String&gt;","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ api-docs/m_captcha/0.1.1/source-files.js
@@ -0,0 +1,5 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["cfg_if"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["m_captcha"] = {"name":"","dirs":[{"name":"cache","files":["hashcache.rs","mod.rs"]}],"files":["defense.rs","errors.rs","lib.rs","master.rs","mcaptcha.rs","pow.rs","system.rs","utils.rs"]};
+sourcesIndex["unicode_xid"] = {"name":"","files":["lib.rs","tables.rs"]};
+createSourceSidebar();
